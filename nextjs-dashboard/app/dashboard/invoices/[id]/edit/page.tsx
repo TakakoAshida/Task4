c8edca0126036437e9ff995817b5a0c91f311467
Takako <taka3000806@gmail.com>
2025-05-26 12:12:06 +0900

    end Chapter12

---
+++ nextjs-dashboard/app/dashboard/invoices/[id]/edit/page.tsx
@@ -0,0 +1,30 @@
+// このフォームには顧客名や請求金額などの情報が事前に入力されている
+import Form from '@/app/ui/invoices/edit-form';
+import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
+import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
+ 
+export default async function Page(props: { params: Promise<{ id: string }> }) {
+    const params = await props.params;
+    const id = params.id;
+    const [invoice, customers] = await Promise.all([
+        // idに基づいて請求書の情報を取得するための関数
+        fetchInvoiceById(id),
+        // 特定の顧客の情報を取得するための関数
+        fetchCustomers(),
+    ]);
+  return (
+    <main>
+      <Breadcrumbs
+        breadcrumbs={[
+          { label: 'Invoices', href: '/dashboard/invoices' },
+          {
+            label: 'Edit Invoice',
+            href: `/dashboard/invoices/${id}/edit`,
+            active: true,
+          },
+        ]}
+      />
+      <Form invoice={invoice} customers={customers} />
+    </main>
+  );
+}
--- nextjs-dashboard/app/lib/actions.ts
+++ nextjs-dashboard/app/lib/actions.ts
@@ -2,8 +2,10 @@
 
 // 型検証にZodという型検証ライブラリを使用
 import { z } from 'zod';
+import { revalidatePath } from 'next/cache';
 import postgres from 'postgres';
-import { SquaresPlusIcon } from '@heroicons/react/24/outline';
+import { redirect } from 'next/navigation';
+// import { SquaresPlusIcon } from '@heroicons/react/24/outline';
 
 const sql = postgres(process.env.POSTGRES_URL!, {ssl: 'require'});
 
@@ -16,19 +18,52 @@ const FormSchema = z.object({
 });
    
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
 export async function createInvoice(formData: FormData) {
     const { customerId, amount, status } =  CreateInvoice.parse({
+        // FormDataから値を取得し、Zodで型検証を行う
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
         status: formData.get('status'),
     });
     // console.log(rawFormData);
+    // 金額をセントに変換
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split('T')[0];
 
+    // SQLクエリを実行してデータベースに新しい請求書を挿入
     await sql`
         INSERT INTO invoices (customer_id, amount, status, date)
         VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
     `;
+
+    // データベースが更新されたあと、パスが再検証されサーバーから最新のデータを取得
+    revalidatePath('/dashboard/invoices');
+    // リダイレクトを行い、ユーザーを請求書の一覧ページに戻す
+    redirect('/dashboard/invoices');
+}
+
+export async function updateInvoice(id: string, formData: FormData) {
+    const { customerId, amount, status } = UpdateInvoice.parse({
+      customerId: formData.get('customerId'),
+      amount: formData.get('amount'),
+      status: formData.get('status'),
+    });
+   
+    const amountInCents = amount * 100;
+   
+    await sql`
+      UPDATE invoices
+      SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+      WHERE id = ${id}
+    `;
+   
+    revalidatePath('/dashboard/invoices');
+    redirect('/dashboard/invoices');
+}
+  
+export async function deleteInvoice(id: string) {
+    await sql`DELETE FROM invoices WHERE id = ${id}`;
+    revalidatePath('/dashboard/invoices');
 }
